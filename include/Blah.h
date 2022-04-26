f1854f2c3b3e7519e9560eddf0f0d242e5f13b1b
Syed Ahmed Zaki <iammzaki@gmail.com>
2022-04-26 23:35:20 +0200

    Learning Cmake

---
+++ include/Blah.h
@@ -0,0 +1,12 @@
+#pragma once
+#include<iostream>
+
+class Blah{
+
+public:
+	inline void foo(){
+	std::cout<<"Boo!\n"<<endl;
+
+	}
+
+};
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include "Blah.h"
+
+
+int main()
+{
+    std::cout<<"hellllooo"<<std::endl;
+    Blah().foo();
+    return 0;
+}
